868274cf10eebe7c4bb844570885b32c3530795e
Ayushh <ayushmishra0228@outlook.com>
2025-04-25 23:46:55 +0530

    nn

---
+++ Streamlit/ML APP /classification.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import sklearn.datasets as load_ris
+import sklearn.ensemble as RFC
+
+
+def load():
+    iris = load()
+    df = pd.DataFrame(iris.data,columns=iris.feature_names)
+    df['species'] = iris.target
+    return df, iris.target_names
+
+
+
+
